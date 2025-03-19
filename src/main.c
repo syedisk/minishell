cd51ae2837b0a15b8d58c66e7f31cb5acba36c85
sbin-ham <sbin-ham@student.42singapore.sg>
2025-03-19 10:44:07 +0800

    added history support; still barebones main;

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sbin-ham <sbin-ham@student.42singapore.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 17:38:43 by sbin-ham          #+#    #+#             */
-/*   Updated: 2025/03/13 17:25:04 by sbin-ham         ###   ########.fr       */
+/*   Updated: 2025/03/19 10:42:48 by sbin-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	main(void)
 		input = readline("minishell$ ");
 		if (!input)
 			break ;
+		if (*input)
+			add_history(input);
 		if (ft_strncmp(input, "exit", 4) == 0 && input[4] == '\0')
 		{
 			free(input);
