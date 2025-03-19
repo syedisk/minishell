ac0b3e1eb9e7e08c44b8901e09ec80c6ed6b58a7
sbin-ham <sbin-ham@student.42singapore.sg>
2025-03-19 13:20:29 +0800

    added free_token, integrated lexer to main

--- src/main.c
+++ src/main.c
@@ -6,16 +6,60 @@
 /*   By: sbin-ham <sbin-ham@student.42singapore.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 17:38:43 by sbin-ham          #+#    #+#             */
-/*   Updated: 2025/03/19 10:42:48 by sbin-ham         ###   ########.fr       */
+/*   Updated: 2025/03/19 13:18:54 by sbin-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
+char **tokens_to_args(t_token *tokens)
+{
+	int	count = 0;
+	t_token *tmp = tokens;
+	char **args;
+
+	while (tmp)
+	{
+		if (tmp->type == WORD)
+			count++;
+		tmp = tmp->next;
+	}
+	args = malloc(sizeof(char *) * (count + 1));
+	if (!args)
+		return (NULL);
+	tmp = tokens;
+	count = 0;
+	while (tmp)
+	{
+		if (tmp->type == WORD)
+			args[count++] = ft_strdup(tmp->value);
+		tmp = tmp->next;
+	}
+	args[count] = NULL;
+	return (args);
+}
+
+void free_tokens(t_token *tokens)
+{
+	t_token *tmp;
+
+	while (tokens)
+	{
+		tmp = tokens->next;
+		free(tokens->value);
+		free(tokens);
+		tokens = tmp;
+	}
+}
+
 int	main(void)
 {
 	char	*input;
-	char	*args[4];
+	t_token *tokens;
+	char	**args;
+	pid_t	pid;
+	int		i;
 
 	while (1)
 	{
@@ -29,15 +73,29 @@ int	main(void)
 			free(input);
 			break ;
 		}
-		args[0] = "/bin/sh";
-		args[1] = "-c";
-		args[2] = input;
-		args[3] = NULL;
-		if (fork() == 0)
-			execve(args[0], args, NULL);
-		else
-			wait(NULL);
+		tokens = tokenise(input);
+		args = token_to_args(tokens);
+		if (args && args[0])
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				execve(args[0], args, NULL); // to add envp?
+				perror("execve");
+				exit(1);
+			}
+			else
+				wait(NULL);
+		}
 		free(input);
+		free_tokens(tokens);
+		i = 0;
+		while (args && args[i])
+		{
+			free(args[i]);
+			i++;
+		}
+		free(args);
 	}
 	return (0);
 }
